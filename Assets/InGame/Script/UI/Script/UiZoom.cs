5a1109a5f23ab0ecf33ed83fe2985f705715b6f4
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-02-24 18:34:16 +0900

    [add]UIのズーム機能の追加

---
+++ Assets/InGame/Script/UI/Script/UiZoom.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiZoom : MonoBehaviour
+{
+    [SerializeField, Tooltip("拡大が完了する時間")] float _zoomTime;
+    [SerializeField, Tooltip("拡大する大きさ")] float _scale;
+    [SerializeField, Tooltip("")]
+    public void ZoomUi(int num)
+    {
+        
+    }
+}
