223f9feb982ab55f6567ce718fb81dec5d298c7d
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-02-25 13:55:02 +0900

    [fix]Uiのズーム機能の修正

--- Assets/InGame/Script/UI/Script/UiZoom.cs
+++ Assets/InGame/Script/UI/Script/UiZoom.cs
@@ -1,20 +1,55 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UiZoom : MonoBehaviour
 {
-    [SerializeField, Tooltip("ズームしたいパネル")] GameObject _zoomPanel;
-    [SerializeField, Tooltip("拡大が完了する時間")] float _zoomTime;
-    [SerializeField, Tooltip("拡大する大きさ")] float _scale;
-    [SerializeField, Tooltip("部位ごとのRectTransform")] RectTransform[] _rectTransforms;
+    [SerializeField, Tooltip("ズームしたいパネル")] private RectTransform _zoomPanel;
+    [SerializeField, Tooltip("拡大が完了する時間")] private float _zoomTime;
+    [SerializeField, Tooltip("拡大する大きさ")] private float _scale;
+    [SerializeField, Tooltip("部位ごとのRectTransform")] private RectTransform[] _rectTransforms;
+    [SerializeField, Tooltip("初期状態のパネル")] private RectTransform _normalPanelSize;
+    /// <summary>パネルが現在ズーム状態かの判定 </summary>
+    private bool IsZoom = false;
+
+    public void OnButtom(int num)
+    {
+        //ズーム状態かにより、条件分岐
+        //ズーム状態ならもとに戻し、それ以外ならズームする
+        if (IsZoom)
+            NormalUi();
+        else
+            ZoomUi(num);
+        IsZoom = !IsZoom;
+    }
 
     /// <summary>
     /// UIをズームするときに呼び出すメソッド
     /// </summary>
     /// <param name="num">部位の添え字</param>
-    public void ZoomUi(int num)
+    private void ZoomUi(int num)
     {
-        
+        //Sequenceの生成
+        var sequence = DOTween.Sequence();
+        //RectTransformでパネルを移動させる
+        sequence.Append(_zoomPanel.DOAnchorPos(new Vector2(_rectTransforms[num].anchoredPosition.x, _rectTransforms[num].anchoredPosition.y), _zoomTime));
+        //ズームさせる
+        sequence.Join(_zoomPanel.DOScale(_scale, _scale));
+        sequence.Play();
+    }
+
+    /// <summary>
+    /// 通常のパネルに戻す
+    /// </summary>
+    private void NormalUi()
+    {
+        //Sequenceの生成
+        var sequence = DOTween.Sequence();
+        //RectTransformでパネルを移動させる
+        sequence.Append(_zoomPanel.DOAnchorPos(new Vector2(_normalPanelSize.anchoredPosition.x, _normalPanelSize.anchoredPosition.y), _zoomTime));
+        //スケールを元に戻す
+        sequence.Join(_zoomPanel.DOScale(1f, 1f));
+        sequence.Play();
     }
 }
