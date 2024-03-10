cb17e5c0d1972735355ea7aafa4e3fae56817699
SatouTappei <stutpi.signup@gmail.com>
2024-03-10 14:22:59 +0900

    [add] 敵作成開始。登場してプレイヤーを追跡、プレイヤーめがけて弾を撃ってくる。

---
+++ Assets/InGame/Enemy/Scripts/DebugUse/SurroundingPoolDebugger.cs
@@ -0,0 +1,30 @@
+﻿using Enemy.Control;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy.DebugUse
+{
+    public class SurroundingPoolDebugger : MonoBehaviour
+    {
+        [SerializeField] private SurroundingPool _pool;
+
+        private Queue<Slot> _q = new Queue<Slot>();
+
+        private void Update()
+        {
+            if (_pool == null) return;
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                if (_pool.TryRent(out Slot s)) _q.Enqueue(s);
+            }
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                if (_q.TryDequeue(out Slot s)) _pool.Return(s);
+            }
+
+            Debug.Log($"空きスロット数:{_pool.EmptySlotCount}");
+        }
+    }
+}
---
+++ Assets/InGame/Enemy/Scripts/Extensions/ColliderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy.Extensions
+{
+    public static class ColliderExtensions
+    {
+        /// <summary>
+        /// 複数のタグで判定する。
+        /// </summary>
+        public static bool CompareTags(this Collider collider, params string[] tags)
+        {
+            foreach (string tag in tags)
+            {
+                if (collider.gameObject.CompareTag(tag)) return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 複数のタグで判定する。
+        /// </summary>
+        public static bool CompareTags(this Collision collision, params string[] tags)
+        {
+            return collision.collider.CompareTags(tags);
+        }
+    }
+}
