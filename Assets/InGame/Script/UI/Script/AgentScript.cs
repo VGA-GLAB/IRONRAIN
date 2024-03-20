48725896b45bd6d30afe5007c7dfe37740f15be2
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-03-20 15:44:43 +0900

    [delete]agentScriptとRaderMapの削除

--- Assets/InGame/Script/UI/Script/AgentScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AgentScript : MonoBehaviour
-{
-    public RectTransform Image;
-    public RaderMap RaderMap;
-}
