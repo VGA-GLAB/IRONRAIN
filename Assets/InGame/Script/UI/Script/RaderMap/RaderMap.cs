a7dcf06df589524d8f8b3690eb63c262e35147f2
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-03-30 18:14:05 +0900

    [fix]レーダーマップの処理を修正

--- Assets/InGame/Script/UI/Script/RaderMap/RaderMap.cs
+++ Assets/InGame/Script/UI/Script/RaderMap/RaderMap.cs
@@ -8,10 +8,11 @@ public class RaderMap : MonoBehaviour
     /// <summary>
     /// 敵のリスト
     /// </summary>
-    private List<AgentScript> _enemys = new List<AgentScript> ();
+    //private List<AgentScript> _enemys = new List<AgentScript> ();
+    private List<GameObject> _enemys = new List<GameObject>();
     [SerializeField, Tooltip("プレイヤーの位置")] private Transform _player;
     [SerializeField, Tooltip("UIの真ん中")] private Image _center;
-    [SerializeField, Tooltip("敵をまとめる親オブジェクト")] private GameObject _dest;
+    //[SerializeField, Tooltip("敵をまとめる親オブジェクト")] private GameObject _dest;
     [SerializeField, Tooltip("レーダーの大きさ")] private float _raderLength = 30f;
     [SerializeField, Tooltip("半径")] private float _radius = 6f;
     [SerializeField, Tooltip("マルチロック距離")] private float _multilockDis = 10f;
@@ -26,28 +27,68 @@ public class RaderMap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //敵をすべて取得する
-        GameObject[] objects = GameObject.FindGameObjectsWithTag("Enemy");
-        foreach(GameObject obj in objects)
-        {
-            AgentScript agent = obj.GetComponent<AgentScript>();
-            agent.RaderMap = this;
-            agent.RectTransform = Instantiate(agent.Image, _center.transform.parent).GetComponent<RectTransform>();
-            _enemys.Add(obj.GetComponent<AgentScript>());
-        }//エネミーを取得する
+        ////敵をすべて取得する
+        //GameObject[] objects = GameObject.FindGameObjectsWithTag("Enemy");
+        //foreach(GameObject obj in objects)
+        //{
+        //    AgentScript agent = obj.GetComponent<AgentScript>();
+        //    agent.RaderMap = this;
+        //    agent.RectTransform = Instantiate(agent.Image, _center.transform.parent).GetComponent<RectTransform>();
+        //    _enemys.Add(obj.GetComponent<AgentScript>());
+        //}//エネミーを取得する
 
-        //_rectTransform = _target.GetComponent<RectTransform>();
+        ////_rectTransform = _target.GetComponent<RectTransform>();
         _offset = _center.GetComponent<RectTransform>().anchoredPosition;
     }
 
-    // Update is called once per frame
+    //void Update()
+    //{
+    //    for(int i = 0; i < _enemys.Count; i++)
+    //    {
+    //        AgentScript agent = _enemy[i]GetComponent<AgentScript>();
+    //        //Imageがなければ生成
+    //        if (!_enemys[i].RectTransform)
+    //            _enemys[i].RectTransform = Instantiate(_enemys[i].Image, _center.transform.parent).GetComponent<RectTransform>();
+
+    //        Vector3 enemyDir = _enemys[i].transform.position;
+    //        //敵の高さとプレイヤーの高さを合わせる
+    //        enemyDir.y = _player.position.y;
+    //        enemyDir = _enemys[i].transform.position - _player.position;
+
+    //        enemyDir = Quaternion.Inverse(_player.rotation) * enemyDir; // ベクトルをプレイヤーに合わせて回転
+    //        enemyDir = Vector3.ClampMagnitude(enemyDir, _raderLength); // ベクトルの長さを制限
+
+    //        //赤点の位置を決める
+    //        _enemys[i].RectTransform.anchoredPosition = new Vector2(enemyDir.x * _radius + _offset.x, enemyDir.z * _radius + _offset.y);
+
+    //        //ロックオンされている場合の処理
+    //        Image image = _enemys[i].RectTransform.GetComponent<Image>();
+    //        if (_enemys[i].IsLockon)
+    //            image.color = Color.blue;
+    //        else
+    //            image.color = Color.red;
+    //    }
+
+
+    //Vector3 enemyDir = _enemy.position;
+    ////敵の高さとプレイヤーの高さを合わせる
+    //enemyDir.y = _player.position.y;
+    //enemyDir = _enemy.position - _player.position;
+
+    //enemyDir = Quaternion.Inverse(_player.rotation) * enemyDir;
+    //enemyDir = Vector3.ClampMagnitude(enemyDir, _raderLength);
+
+    //_rectTransform.anchoredPosition = new Vector2(enemyDir.x * _radius + _offset.x, enemyDir.z * _radius + _offset.y);
+    // }
+
     void Update()
     {
-        for(int i = 0; i < _enemys.Count; i++)
+        for (int i = 0; i < _enemys.Count; i++)
         {
+            AgentScript agent = _enemys[i].GetComponent<AgentScript>();
             //Imageがなければ生成
-            if (!_enemys[i].RectTransform)
-                _enemys[i].RectTransform = Instantiate(_enemys[i].Image, _center.transform.parent).GetComponent<RectTransform>();
+            if (!agent.RectTransform)
+                agent.RectTransform = Instantiate(agent.Image, _center.transform.parent).GetComponent<RectTransform>();
 
             Vector3 enemyDir = _enemys[i].transform.position;
             //敵の高さとプレイヤーの高さを合わせる
@@ -58,26 +99,37 @@ public class RaderMap : MonoBehaviour
             enemyDir = Vector3.ClampMagnitude(enemyDir, _raderLength); // ベクトルの長さを制限
 
             //赤点の位置を決める
-            _enemys[i].RectTransform.anchoredPosition = new Vector2(enemyDir.x * _radius + _offset.x, enemyDir.z * _radius + _offset.y);
+            agent.RectTransform.anchoredPosition = new Vector2(enemyDir.x * _radius + _offset.x, enemyDir.z * _radius + _offset.y);
 
             //ロックオンされている場合の処理
-            Image image = _enemys[i].RectTransform.GetComponent<Image>();
-            if (_enemys[i].IsLockon)
+            Image image = agent.RectTransform.GetComponent<Image>();
+            if (agent.IsLockon)
                 image.color = Color.blue;
             else
                 image.color = Color.red;
         }
+    }
 
+    /// <summary>
+    /// エネミーが生成された時に呼ぶスクリプト
+    /// </summary>
+    /// <param name="enemy">エネミーオブジェクト</param>
+    public void GenerateEnemy(GameObject enemy)
+    {
+        AgentScript agent = enemy.GetComponent<AgentScript>();
+        agent.RaderMap = this;
+        agent.RectTransform = Instantiate(agent.Image, _center.transform.parent).GetComponent<RectTransform>();
+        //_enemys.Add(enemy.GetComponent<AgentScript>());
+        _enemys.Add(enemy.GetComponent<GameObject>());
+    }
 
-        //Vector3 enemyDir = _enemy.position;
-        ////敵の高さとプレイヤーの高さを合わせる
-        //enemyDir.y = _player.position.y;
-        //enemyDir = _enemy.position - _player.position;
-
-        //enemyDir = Quaternion.Inverse(_player.rotation) * enemyDir;
-        //enemyDir = Vector3.ClampMagnitude(enemyDir, _raderLength);
-
-        //_rectTransform.anchoredPosition = new Vector2(enemyDir.x * _radius + _offset.x, enemyDir.z * _radius + _offset.y);
+    /// <summary>
+    /// エネミーが倒された時に呼ばれるメソッド
+    /// </summary>
+    /// <param name="enemy"></param>
+    public void DestroyEnemy(GameObject enemy)
+    {
+        
     }
 
     /// <summary>
@@ -109,7 +161,8 @@ public class RaderMap : MonoBehaviour
         //全てのエネミーのロックオンを外す
         foreach(var enemy in _enemys)
         {
-            enemy.IsLockon = false;
+            var agent = enemy.GetComponent<AgentScript>();
+            agent.IsLockon = false;
         }
         
         var nearEnemy = NearEnemy();
@@ -125,7 +178,8 @@ public class RaderMap : MonoBehaviour
         //全てのエネミーのロックオンを外す
         foreach (var enemy in _enemys)
         {
-            enemy.IsLockon = false;
+            var agent = enemy.GetComponent<AgentScript>();
+            agent.IsLockon = false;
         }
 
         //エネミーとの距離を判定する
