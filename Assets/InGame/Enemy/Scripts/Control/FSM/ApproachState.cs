8d7672b7a92fdec9a6f008e9171bd30ba587fc42
SatouTappei <stutpi.signup@gmail.com>
2024-04-26 13:32:02 +0900

    [fix] 接近中は敵が死なないバグを修正

--- Assets/InGame/Enemy/Scripts/Control/FSM/ApproachState.cs
+++ Assets/InGame/Enemy/Scripts/Control/FSM/ApproachState.cs
@@ -34,6 +34,30 @@ namespace Enemy.Control.FSM
 
         protected override void Stay(IReadOnlyDictionary<StateKey, State> stateTable)
         {
+            if (IsDead())
+            {
+                TryChangeState(stateTable[StateKey.Idle]);
+            }
+            else
+            {
+                Approach(stateTable);
+            }  
+        }
+
+        // 死んだかチェック
+        private bool IsDead()
+        {
+            foreach (ActionPlan plan in _blackBoard.ActionOptions)
+            {
+                if (plan.Choice == Choice.Broken) return true;
+            }
+
+            return false;
+        }
+
+        // 移動させ、移動完了した場合は遷移
+        private void Approach(IReadOnlyDictionary<StateKey, State> stateTable)
+        {
             // スロット位置への接近中かの判定がこのステートの終了条件。
             // 接近するための移動や回転を行わないフレームがあれば条件を満たして遷移。
             bool isApproaching = false;
