4c7f5c1c9516276e1c17a85c17d285e4716a4921
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-05-14 23:53:53 +0900

    [fix]マルチロックシステムの修正

--- Assets/InGame/Script/UI/Script/Rockon/MultilockSystem.cs
+++ Assets/InGame/Script/UI/Script/Rockon/MultilockSystem.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class MultilockSystem : MonoBehaviour
 {
+    [SerializeField, Tooltip("Rayを飛ばす起点")] GameObject _rayOrigin;
     /// <summary>マルチロック中であるか </summary>
     public bool IsMultilock { get; private set; }
     /// <summary>敵のUIリスト </summary>
     public List<EnemyUi> EnemyUis;
     /// <summary>敵のUIリスト（テスト用） </summary>
-    public List<AgentScript> AgentScripts;
-
+    public List<GameObject> LockOnEnemy;
+    [SerializeField, Tooltip("Rayのレイヤーマスク")] LayerMask _layerMask;
     
     // Update is called once per frame
     private void Update()
@@ -34,6 +37,23 @@ public class MultilockSystem : MonoBehaviour
     /// </summary>
     private void SerchEnemy()
     {
+        //Rayを飛ばすスタート位置を決める
+        var rayStartPosition = _rayOrigin.transform.position;
+        //マウスでRayを飛ばす方向を決める
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Hitしたオブジェクト格納用
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, _layerMask))
+        {
+            if(hit.collider.gameObject.tag == "Enemy")
+            {
+                LockOnEnemy.Add(hit.collider.gameObject);
+            }
+        }
+    }
 
+    private void MultiLock()
+    {
+        LockOnEnemy = LockOnEnemy.Distinct().ToList();
     }
 }
