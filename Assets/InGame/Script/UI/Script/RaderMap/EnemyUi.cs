2c147442fbf8af55c462d803e8b4924bdd32ad96
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-05-23 21:52:21 +0900

    [update]Test、エネミーUiの更新

--- Assets/InGame/Script/UI/Script/RaderMap/EnemyUi.cs
+++ Assets/InGame/Script/UI/Script/RaderMap/EnemyUi.cs
@@ -3,20 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class EnemyUi : MonoBehaviour, IPointerDownHandler
 {
     public GameObject Enemy;
     /// <summary>表示するレーダーマップ </summary>
     [NonSerialized] public RaderMap RaderMap;
+    private Image _image;
 
     private void Awake()
     {
         //レーダーテストを検索する
         RaderMap = GameObject.Find("RaderTest").GetComponent<RaderMap>();
     }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         RaderMap.PanelRock(Enemy);
+    }
+
+    /// <summary>
+    /// マルチロック時に呼び出される処理
+    /// </summary>
+    public void MultiLock()
+    {
+        
     }
 }
