ebe2b68aea742ddf1e2df4ba91dd7c0dce40a099
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-06-14 16:13:04 +0900

    [add] 攻撃チュートリアル

--- Assets/InGame/Script/EventSequence/Sequence/InGame/Chase/AttackSequence.cs
+++ Assets/InGame/Script/EventSequence/Sequence/InGame/Chase/AttackSequence.cs
@@ -1,9 +1,56 @@
+﻿using UnityEngine;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using Enemy.Control;
 
 public sealed class AttackSequence : AbstractSequenceBase
 {
+    [SerializeField] private TutorialTextBoxController _textBox = default;
+    [SerializeField] private EnemyManager _enemyManager = default;
+
+    [Header("テキストボックスに関する値")]
+    [SerializeField] private float _textBoxOpenAndCloseSec = 0.5F;
+    [SerializeField, TextArea] private string _tutorialText = "現在、テストテキストが表示されています";
+    [SerializeField] private float _textDuration = 3F;
+
+    [Header("アナウンス音声に関する値")]
+    [SerializeField] private string _announceCueSheetName = "SE";
+    [SerializeField] private string _announceCueName = "SE_Kill";
+    [SerializeField] private float _waitAfterAnnounceSec = 1F;
+
     public async override UniTask PlaySequenceAsync(CancellationToken ct)
     {
+        // 最初の待ち時間
+        await UniTask.WaitForSeconds(2F, cancellationToken: ct);
+
+        // 攻撃の仕方をオペレーターが喋る
+        await Announce(ct);
+
+        // 実際に1機倒す
+        await UniTask.WaitUntil(() => _enemyManager.IsAllDefeated(EnemyManager.Sequence.Attack));
+    }
+
+    /// <summary>アナウンスとTextBoxの更新を同時に行う関数</summary>
+    /// <param name="cancellationToken"></param>
+    private async UniTask Announce(CancellationToken cancellationToken)
+    {
+        await _textBox.DoOpenTextBoxAsync(_textBoxOpenAndCloseSec, cancellationToken);
+
+        _textBox.ClearText();
+
+        await UniTask.WhenAll(
+            CriAudioManager.Instance.SE.PlayAsync(_announceCueSheetName, _announceCueName, cancellationToken),
+            ChangeText()
+        );
+
+        await _textBox.DoCloseTextBoxAsync(_textBoxOpenAndCloseSec, cancellationToken);
+
+        _textBox.ClearText();
+
+        async UniTask ChangeText()
+        {
+            await _textBox.DoTextChangeAsync(_tutorialText, _textDuration, cancellationToken);
+            await UniTask.WaitForSeconds(_waitAfterAnnounceSec, cancellationToken: cancellationToken);
+        }
     }
 }
