00a818b9521967948b5effbea2e14ba25f30c65a
Tappei <stutpi.signup@gmail.com>
2024-06-21 20:25:57 +0900

    [fix] 消えたものを戻した。

---
+++ Assets/InGame/Enemy/Scripts/Control_Enemy/BT/HideIfOffScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy.Control.BT
+{
+    public class HideIfOffScreen : Node
+    {
+        private ActionPlan _plan;
+        private BlackBoard _blackBoard;
+
+        public HideIfOffScreen(BlackBoard blackBoard)
+        {
+            _plan = new ActionPlan(Choice.Hide);
+            _blackBoard = blackBoard;
+        }
+
+        protected override void Enter()
+        {
+        }
+
+        protected override void Exit()
+        {
+        }
+
+        protected override State Stay()
+        {
+            // とりあえずプレイヤーとの距離で判定。
+            if (_blackBoard.TransformToPlayerDistance > 20.0f) // 値は適当。
+            {
+                _blackBoard.ActionPlans.Enqueue(_plan);
+            }
+
+            return State.Success;
+        }
+    }
+}
