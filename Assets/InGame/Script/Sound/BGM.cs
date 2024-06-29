427951768d15911c4def196585b6e0db568e872e
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-06-29 20:35:15 +0900

    [add] BGMを再生するスクリプト

---
+++ Assets/InGame/Script/Sound/BGM.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>追跡パートのBGM</summary>
+public class BGM : MonoBehaviour
+{
+    [SerializeField, Header("BGMを再生するか")] 
+    private bool _isPlay = false;
+
+    [SerializeField, Header("BGMの音量"), Range(0, 1.0f)] 
+    private float _volume = 1.0f;
+
+    void Start()
+    {
+        if( _isPlay )
+        {
+            CriSoundManager.Instance.BGM.Play("BGM", "BGM_Field", _volume);
+        }
+    }
+}
