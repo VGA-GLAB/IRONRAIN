6b15e877f2b7b1ca57d46d89ea31b7aec02ffea5
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-06-29 19:33:00 +0900

    [add] 追跡パートのBGM（月面フィールド（BGM））を再生するオブジェクトを作成。

---
+++ Assets/InGame/Script/Sound/ChaseSceneBGM.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>追跡パートのBGM</summary>
+public class ChaseSceneBGM : MonoBehaviour
+{
+    [SerializeField,Header("BGMを再生するか")] 
+    private bool _isPlay = false;
+    
+    [SerializeField, Header("BGMの音量"), Range(0, 1)]
+    private float _volume = 0;
+
+    void Start()
+    {
+        if (_isPlay) 
+            CriSoundManager.Instance.BGM.Play("BGM", "BGM_Field", _volume);
+    }
+}
