4adec94466d64be9f0d9b07f94213586204083c1
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-08-04 10:24:34 +0900

    [update]マップ生成処理の更新

--- Assets/InGame/Script/Map/MapManager.cs
+++ Assets/InGame/Script/Map/MapManager.cs
@@ -8,22 +8,40 @@ public class MapManager : MonoBehaviour
     private Transform _playerTransform;
     [Header("マップオブジェクトリスト"), SerializeField, Tooltip("マップオブジェクトリスト")]
     private List<GameObject> _mapObjectsList;
-
     [Header("チェックポイントリスト"), SerializeField, Tooltip("チェックポイントリスト")]
     private List<Transform> _checkPointList;
-    
-   // [Header("")]
+
+    /// <summary>チェックポイントのカウンター</summary>
+    private int _checkPointCount;
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         //初期表示のマップをアクティブにする
-        
+        for (int i = 0; i < _mapObjectsList.Count; i++)
+        {
+            if (i < 2)
+                _mapObjectsList[i].SetActive(true);
+            else
+                _mapObjectsList[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //マップを進める
+        if (_checkPointList[_checkPointCount].position.z >= _playerTransform.position.z)
+            ChangeMap();
+    }
 
+    /// <summary>
+    /// マップの入れ替えをする処理
+    /// </summary>
+    private void ChangeMap()
+    {
+        _mapObjectsList[_checkPointCount].SetActive(false);
+        _mapObjectsList[_checkPointCount + 2].SetActive(true);
+        _checkPointCount++;
     }
 }
