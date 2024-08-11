7322119be45ccab0f54b99bccc527f63f1b1cae3
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-08-11 12:07:31 +0900

    [add]アナウンスUi処理を追加

---
+++ Assets/InGame/Script/UI/Script/Anouunce/AnnounceUiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum AnnounceUiType
+{
+    NoImage,
+    Operator,
+    Ryan
+}
+
+public class AnnounceUiController : MonoBehaviour
+{
+    //[Header("アナウンスUiオブジェクト")]
+    //[SerializeField] private GameObject 
+    /// <summary>
+    /// アナウンスパネルを切り替える
+    /// </summary>
+    /// <param name="announceUiType">変更後のアナウンス状態</param>
+    public void ChangeAnnounceUi(AnnounceUiType announceUiType)
+    {
+
+    }
+}
