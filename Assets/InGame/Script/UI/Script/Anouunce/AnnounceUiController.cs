0a59912ce7d884bf9d70e1daeebcebd22b12a0de
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-08-17 20:57:59 +0900

    [add]Ui素材の追加、アナウンスパネルの処理とプレハブの追加

--- Assets/InGame/Script/UI/Script/Anouunce/AnnounceUiController.cs
+++ Assets/InGame/Script/UI/Script/Anouunce/AnnounceUiController.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using Cysharp.Threading.Tasks;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 
@@ -12,14 +15,56 @@ public enum AnnounceUiType
 
 public class AnnounceUiController : MonoBehaviour
 {
-    //[Header("アナウンスUiオブジェクト")]
-    //[SerializeField] private GameObject 
+    [Header("オペレーダーUi")]
+    [SerializeField] private GameObject _operatorUi;
+    [Header("ライアンUi")]
+    [SerializeField] private GameObject _ryanUi;
+    [Header("NoImageUi")]
+    [SerializeField] private GameObject _noImageUi;
+
+    [Header("音が鳴ってからパネルが切り替わるまでの時間")] 
+    [SerializeField] private double _uiChangeInterval;
     /// <summary>
     /// アナウンスパネルを切り替える
     /// </summary>
     /// <param name="announceUiType">変更後のアナウンス状態</param>
-    public void ChangeAnnounceUi(AnnounceUiType announceUiType)
+    public async UniTaskVoid ChangeAnnounceUi(AnnounceUiType announceUiType)
     {
+        //前兆音を鳴らす
+        CriAudioManager.Instance.SE.Play("SE", "SE_Panel");
+        
+        //数秒待つ
+        await UniTask.Delay(TimeSpan.FromSeconds(_uiChangeInterval));
+        
+        switch(announceUiType)
+        {
+            case AnnounceUiType.NoImage:
+                _noImageUi.transform.SetAsLastSibling();
+                break;
+            case AnnounceUiType.Operator:
+                _operatorUi.transform.SetAsLastSibling();
+                break ;
+            case AnnounceUiType.Ryan:
+                _ryanUi.transform.SetAsLastSibling();
+                break;
+        }
+    }
 
+    //////テスト用/////
+
+
+    public void TestRyan()
+    {
+        ChangeAnnounceUi(AnnounceUiType.Ryan);
+    }
+
+    public void TestNoImage()
+    {
+        ChangeAnnounceUi(AnnounceUiType.NoImage);
+    }
+
+    public void TestOperator()
+    {
+        ChangeAnnounceUi(AnnounceUiType.Operator);
     }
 }
