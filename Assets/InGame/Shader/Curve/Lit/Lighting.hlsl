11ba9f71340f2b89455624eb68103dbc876e2b4f
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2024-08-18 16:22:12 +0900

    [add] CurveShaderの追加

---
+++ Assets/InGame/Shader/Curve/Lit/Lighting.hlsl
@@ -0,0 +1,213 @@
+#ifndef CUSTOM_CURVE_TERRAIN_LIGHTING_INCLUDED
+#define CUSTOM_CURVE_TERRAIN_LIGHTING_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/CommonMaterial.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/SurfaceInput.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/ParallaxMapping.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DBuffer.hlsl"
+#include "Header.hlsl"
+
+void ApplyPerPixelDisplacement(half3 viewDirTS, inout float2 uv)
+{
+    #if defined(_PARALLAXMAP)
+    uv += ParallaxMapping(TEXTURE2D_ARGS(_ParallaxMap, sampler_ParallaxMap), viewDirTS, _Parallax, uv);
+    #endif
+}
+
+void InitializeInputData(Varyings input, half3 normalTS, out InputData inputData)
+{
+    inputData = (InputData)0;
+
+#if defined(REQUIRES_WORLD_SPACE_POS_INTERPOLATOR)
+    inputData.positionWS = input.PositionWS;
+#endif
+
+    half3 viewDirWS = GetWorldSpaceNormalizeViewDir(input.PositionWS);
+#if defined(_NORMALMAP) || defined(_DETAIL)
+    float sgn = input.TangentWS.w;      // should be either +1 or -1
+    float3 bitangent = sgn * cross(input.NormalWS.xyz, input.TangentWS.xyz);
+    half3x3 tangentToWorld = half3x3(input.TangentWS.xyz, bitangent.xyz, input.NormalWS.xyz);
+
+    #if defined(_NORMALMAP)
+    inputData.tangentToWorld = tangentToWorld;
+    #endif
+    inputData.normalWS = TransformTangentToWorld(normalTS, tangentToWorld);
+#else
+    inputData.normalWS = input.NormalWS;
+#endif
+
+    inputData.normalWS = NormalizeNormalPerPixel(inputData.normalWS);
+    inputData.viewDirectionWS = viewDirWS;
+
+#if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+    inputData.shadowCoord = input.shadowCoord;
+#elif defined(MAIN_LIGHT_CALCULATE_SHADOWS)
+    inputData.shadowCoord = TransformWorldToShadowCoord(inputData.positionWS);
+#else
+    inputData.shadowCoord = float4(0, 0, 0, 0);
+#endif
+#ifdef _ADDITIONAL_LIGHTS_VERTEX
+    inputData.fogCoord = InitializeInputDataFog(float4(input.positionWS, 1.0), input.fogFactorAndVertexLight.x);
+    inputData.vertexLighting = input.fogFactorAndVertexLight.yzw;
+#else
+    inputData.fogCoord = InitializeInputDataFog(float4(input.PositionWS, 1.0), input.fogFactor);
+#endif
+
+#if defined(DYNAMICLIGHTMAP_ON)
+    inputData.bakedGI = SAMPLE_GI(input.staticLightmapUV, input.dynamicLightmapUV, input.vertexSH, inputData.normalWS);
+#else
+    inputData.bakedGI = SAMPLE_GI(input.staticLightmapUV, input.vertexSH, inputData.normalWS);
+#endif
+
+    inputData.normalizedScreenSpaceUV = GetNormalizedScreenSpaceUV(input.positionCS);
+    inputData.shadowMask = SAMPLE_SHADOWMASK(input.staticLightmapUV);
+
+    #if defined(DEBUG_DISPLAY)
+    #if defined(DYNAMICLIGHTMAP_ON)
+    inputData.dynamicLightmapUV = input.dynamicLightmapUV;
+    #endif
+    #if defined(LIGHTMAP_ON)
+    inputData.staticLightmapUV = input.staticLightmapUV;
+    #else
+    inputData.vertexSH = input.vertexSH;
+    #endif
+    #endif
+}
+
+half4 SampleMetallicSpecGloss(float2 uv, half albedoAlpha)
+{
+    half4 specGloss;
+
+    #ifdef _METALLICSPECGLOSSMAP
+    specGloss = half4(SAMPLE_METALLICSPECULAR(uv));
+    #ifdef _SMOOTHNESS_TEXTURE_ALBEDO_CHANNEL_A
+    specGloss.a = albedoAlpha * _Smoothness;
+    #else
+    specGloss.a *= _Smoothness;
+    #endif
+    #else // _METALLICSPECGLOSSMAP
+    #if _SPECULAR_SETUP
+    specGloss.rgb = _SpecColor.rgb;
+    #else
+    specGloss.rgb = _Metallic.rrr;
+    #endif
+
+    #ifdef _SMOOTHNESS_TEXTURE_ALBEDO_CHANNEL_A
+    specGloss.a = albedoAlpha * _Smoothness;
+    #else
+    specGloss.a = _Smoothness;
+    #endif
+    #endif
+
+    return specGloss;
+}
+
+half SampleOcclusion(float2 uv)
+{
+    #ifdef _OCCLUSIONMAP
+    half occ = SAMPLE_TEXTURE2D(_OcclusionMap, sampler_OcclusionMap, uv).g;
+    return LerpWhiteTo(occ, _OcclusionStrength);
+    #else
+    return half(1.0);
+    #endif
+}
+
+
+// Returns clear coat parameters
+// .x/.r == mask
+// .y/.g == smoothness
+half2 SampleClearCoat(float2 uv)
+{
+    #if defined(_CLEARCOAT) || defined(_CLEARCOATMAP)
+    half2 clearCoatMaskSmoothness = half2(_ClearCoatMask, _ClearCoatSmoothness);
+
+    #if defined(_CLEARCOATMAP)
+    clearCoatMaskSmoothness *= SAMPLE_TEXTURE2D(_ClearCoatMap, sampler_ClearCoatMap, uv).rg;
+    #endif
+
+    return clearCoatMaskSmoothness;
+    #else
+    return half2(0.0, 1.0);
+    #endif  // _CLEARCOAT
+}
+
+half3 ApplyDetailAlbedo(float2 detailUv, half3 albedo, half detailMask)
+{
+    #if defined(_DETAIL)
+    half3 detailAlbedo = SAMPLE_TEXTURE2D(_DetailAlbedoMap, sampler_DetailAlbedoMap, detailUv).rgb;
+
+    // In order to have same performance as builtin, we do scaling only if scale is not 1.0 (Scaled version has 6 additional instructions)
+    #if defined(_DETAIL_SCALED)
+    detailAlbedo = ScaleDetailAlbedo(detailAlbedo, _DetailAlbedoMapScale);
+    #else
+    detailAlbedo = half(2.0) * detailAlbedo;
+    #endif
+
+    return albedo * LerpWhiteTo(detailAlbedo, detailMask);
+    #else
+    return albedo;
+    #endif
+}
+
+half3 ApplyDetailNormal(float2 detailUv, half3 normalTS, half detailMask)
+{
+    #if defined(_DETAIL)
+    #if BUMP_SCALE_NOT_SUPPORTED
+    half3 detailNormalTS = UnpackNormal(SAMPLE_TEXTURE2D(_DetailNormalMap, sampler_DetailNormalMap, detailUv));
+    #else
+    half3 detailNormalTS = UnpackNormalScale(SAMPLE_TEXTURE2D(_DetailNormalMap, sampler_DetailNormalMap, detailUv), _DetailNormalMapScale);
+    #endif
+
+    // With UNITY_NO_DXT5nm unpacked vector is not normalized for BlendNormalRNM
+    // For visual consistancy we going to do in all cases
+    detailNormalTS = normalize(detailNormalTS);
+
+    return lerp(normalTS, BlendNormalRNM(normalTS, detailNormalTS), detailMask); // todo: detailMask should lerp the angle of the quaternion rotation, not the normals
+    #else
+    return normalTS;
+    #endif
+}
+
+inline void InitializeStandardLitSurfaceData(float2 uv, out SurfaceData outSurfaceData)
+{
+    half4 albedoAlpha = SampleAlbedoAlpha(uv, TEXTURE2D_ARGS(_BaseMap, sampler_BaseMap));
+    outSurfaceData.alpha = Alpha(albedoAlpha.a, _BaseColor, _Cutoff);
+
+    half4 specGloss = SampleMetallicSpecGloss(uv, albedoAlpha.a);
+    outSurfaceData.albedo = albedoAlpha.rgb * _BaseColor.rgb;
+    outSurfaceData.albedo = AlphaModulate(outSurfaceData.albedo, outSurfaceData.alpha);
+
+    #if _SPECULAR_SETUP
+    outSurfaceData.metallic = half(1.0);
+    outSurfaceData.specular = specGloss.rgb;
+    #else
+    outSurfaceData.metallic = specGloss.r;
+    outSurfaceData.specular = half3(0.0, 0.0, 0.0);
+    #endif
+
+    outSurfaceData.smoothness = specGloss.a;
+    outSurfaceData.normalTS = SampleNormal(uv, TEXTURE2D_ARGS(_BumpMap, sampler_BumpMap), _BumpScale);
+    outSurfaceData.occlusion = SampleOcclusion(uv);
+    outSurfaceData.emission = SampleEmission(uv, _EmissionColor.rgb, TEXTURE2D_ARGS(_EmissionMap, sampler_EmissionMap));
+
+    #if defined(_CLEARCOAT) || defined(_CLEARCOATMAP)
+    half2 clearCoat = SampleClearCoat(uv);
+    outSurfaceData.clearCoatMask       = clearCoat.r;
+    outSurfaceData.clearCoatSmoothness = clearCoat.g;
+    #else
+    outSurfaceData.clearCoatMask       = half(0.0);
+    outSurfaceData.clearCoatSmoothness = half(0.0);
+    #endif
+
+    #if defined(_DETAIL)
+    half detailMask = SAMPLE_TEXTURE2D(_DetailMask, sampler_DetailMask, uv).a;
+    float2 detailUv = uv * _DetailAlbedoMap_ST.xy + _DetailAlbedoMap_ST.zw;
+    outSurfaceData.albedo = ApplyDetailAlbedo(detailUv, outSurfaceData.albedo, detailMask);
+    outSurfaceData.normalTS = ApplyDetailNormal(detailUv, outSurfaceData.normalTS, detailMask);
+    #endif
+}
+
+
+#endif
---
+++ Assets/InGame/Shader/Curve/Terrain/Passes/CurveTerrainShadowPass.hlsl
@@ -0,0 +1,74 @@
+#ifndef CUSTOM_CURVE_TERRAIN_SHADOW_CASTER_PASS_INCLUDED
+#define CUSTOM_CURVE_TERRAIN_SHADOW_CASTER_PASS_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+#include "../Header.hlsl"
+#include "../../VertexCurve.hlsl"
+
+float3 _LightDirection;
+float3 _LightPosition;
+
+void TerrainInstancing(inout float4 positionOS, inout float3 normal, inout float2 uv)
+{
+    #ifdef UNITY_INSTANCING_ENABLED
+    float2 patchVertex = positionOS.xy;
+    float4 instanceData = UNITY_ACCESS_INSTANCED_PROP(Terrain, _TerrainPatchInstanceData);
+
+    float2 sampleCoords = (patchVertex.xy + instanceData.xy) * instanceData.z; // (xy + float2(xBase,yBase)) * skipScale
+    float height = UnpackHeightmap(_TerrainHeightmapTexture.Load(int3(sampleCoords, 0)));
+
+    positionOS.xz = sampleCoords * _TerrainHeightmapScale.xz;
+    positionOS.y = height * _TerrainHeightmapScale.y;
+
+    #ifdef ENABLE_TERRAIN_PERPIXEL_NORMAL
+    normal = float3(0, 1, 0);
+    #else
+    normal = _TerrainNormalmapTexture.Load(int3(sampleCoords, 0)).rgb * 2 - 1;
+    #endif
+    uv = sampleCoords * _TerrainHeightmapRecipSize.zw;
+    #endif
+}
+
+Varyings CurveTerrainShadowCasterVertex(Attributes input)
+{
+    Varyings o = (Varyings)0;
+    UNITY_SETUP_INSTANCE_ID(v);
+    TerrainInstancing(input.PositionOS, input.NormalOS, input.UV0);
+
+    VertexPositionInputs curvedPositionInput;
+    CalcVertexCurve(_CurveFactor, _CurveOffset, _CurveStrength, GetVertexPositionInputs(input.PositionOS), curvedPositionInput);
+    
+    float3 positionWS = curvedPositionInput.positionWS;
+    float3 normalWS = TransformObjectToWorldNormal(input.NormalOS);
+
+    #if _CASTING_PUNCTUAL_LIGHT_SHADOW
+    float3 lightDirectionWS = normalize(_LightPosition - positionWS);
+    #else
+    float3 lightDirectionWS = _LightDirection;
+    #endif
+
+    float4 clipPos = TransformWorldToHClip(ApplyShadowBias(positionWS, normalWS, lightDirectionWS));
+
+    #if UNITY_REVERSED_Z
+    clipPos.z = min(clipPos.z, UNITY_NEAR_CLIP_VALUE);
+    #else
+    clipPos.z = max(clipPos.z, UNITY_NEAR_CLIP_VALUE);
+    #endif
+
+    o.PositionCS = clipPos;
+
+    o.UV01 = input.UV0;
+
+    return o;
+}
+
+half4 TerrainLitShadowCasterFragment(Varyings input) : SV_Target
+{
+    #ifdef _ALPHATEST_ON
+    ClipHoles(IN.texcoord);
+    #endif
+    return 0;
+}
+
+#endif
