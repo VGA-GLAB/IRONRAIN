802f16105bc80926878e374dcd8c96437a81f0fa
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2024-08-21 19:22:15 +0900

    [fix] ひびの追加

--- Assets/InGame/Script/Shader/WindowMaterialController.cs
+++ Assets/InGame/Script/Shader/WindowMaterialController.cs
@@ -11,6 +11,16 @@ namespace IronRain.ShaderSystem
 
         private int _crackAmountPropertyID = Shader.PropertyToID("_CrackAmount");
 
+        /// <summary>日々の形基本的に1から4にかけて日々が増える</summary>
+        public enum CrackType
+        {
+            None,
+            Type1,
+            Type2,
+            Type3,
+            Type4
+        }
+
         private void Awake()
         {
             _materials = Array.ConvertAll(GetComponentsInChildren<Renderer>(), renderer => renderer.material);
@@ -25,6 +35,17 @@ namespace IronRain.ShaderSystem
             foreach (var mat in _materials)
             {
                 mat?.SetFloat(_crackAmountPropertyID, crackRatio);
+            }
+        }
+
+        public void Crack(CrackType crackType)
+        {
+            // Todo: テクスチャが来たらそれを差し替える処理に変更する
+            if (crackType is CrackType.None) return;
+            
+            foreach (var mat in _materials)
+            {
+                mat?.SetFloat(_crackAmountPropertyID, 0.5F);
             }
         }
     }
