3b9bf5f79d58f018150d9e16cf3e1904e8f22e15
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2024-08-24 14:16:35 +0900

    [add] UIのAnimationをSequenceに追加

---
+++ Assets/InGame/Script/Sequence System/Sequence/HangerUIAnimationSequence.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace IronRain.SequenceSystem
+{
+    public class HangerUIAnimationSequence : ISequence
+    {
+        private enum AnimType
+        {
+            StartActive,
+            ButtonActive,
+            WaitLaunch
+        }
+
+        [OpenScriptButton(typeof(HangerUIAnimationSequence))]
+        [Description("UIのAnimationを呼び出すSequence")]
+        [SerializeField, Header("Animationのタイプ")]
+        private AnimType _animType;
+        
+        private LaunchManager _launchManager;
+        
+        public void SetData(SequenceData data)
+        {
+            _launchManager = data.LaunchManager;
+        }
+
+        public async UniTask PlayAsync(CancellationToken ct, Action<Exception> exceptionHandler = null)
+        {
+            switch (_animType)
+            {
+                case AnimType.StartActive:
+                {
+                    await _launchManager.StartActivateUi(ct);
+                    break;
+                }
+                case AnimType.ButtonActive:
+                {
+                    _launchManager.ButtonActive();
+                    await UniTask.CompletedTask;
+                    break;
+                }
+                case AnimType.WaitLaunch:
+                {
+                    await _launchManager.WaitLaunchAnimation(ct);
+                    break;
+                }
+                default:
+                {
+                    await UniTask.CompletedTask;
+                    break;
+                }
+            }
+        }
+
+        public void Skip()
+        {
+        }
+    }
+}
