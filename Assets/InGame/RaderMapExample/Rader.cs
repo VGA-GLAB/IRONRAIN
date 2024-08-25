63f3ca8778566afab4524588865f6292ceaae003
Tappei <stutpi.signup@gmail.com>
2024-08-25 22:50:25 +0900

    [add] レーダーマップのサンプル

---
+++ Assets/InGame/RaderMapExample/Rader.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RaderMapExample
+{
+    public class Rader : MonoBehaviour
+    {
+        [SerializeField] private float _radius = 9.0f;
+
+        private Collider[] _result;
+        private Transform _transform;
+
+        // レーダーに捉えられる限界、適当。
+        public int Capacity => 10;
+
+        private void Start()
+        {
+            _result = new Collider[Capacity];
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            _result = Physics.OverlapSphere(_transform.position, _radius);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!Application.isPlaying) return;
+
+            // レーダーの範囲
+            Gizmos.color = new Color(1, 0, 0, 0.1f);
+            Gizmos.DrawSphere(GetCenter(), _radius);
+
+            // レーダーに捉えた対象
+            Gizmos.color = Color.red;
+            foreach (Collider c in _result)
+            {
+                Vector3 p = c.transform.position;
+                Gizmos.DrawCube(p, Vector3.one);
+            }
+        }
+
+        /// <summary>
+        /// レーダーの中心位置を返す。
+        /// </summary>
+        public Vector3 GetCenter()
+        {
+            return _transform.position;
+        }
+
+        /// <summary>
+        /// 補足した対象ごとに、レーダーの中心位置からのベクトルを返す。
+        /// </summary>
+        public IEnumerable<Vector3> GetResultVec3()
+        {
+            foreach (Collider c in _result)
+            {
+                if (c == null) yield break;
+
+                Vector3 a = c.transform.position;
+                a.y = 0;
+                Vector3 b = GetCenter();
+                b.y = 0;
+
+                yield return a - b; 
+            }
+        }
+
+        /// <summary>
+        /// 補足した対象ごとに、レーダーの中心位置からのベクトルを返す。
+        /// UIに表示させるよう、XY平面上のベクトルに加工してある。
+        /// </summary>
+        public IEnumerable<Vector2> GetResultVec2()
+        {
+            foreach (Vector3 v in GetResultVec3())
+            {
+                yield return new Vector2(v.x, v.z);
+            }
+        }
+    }
+}
---
+++ Assets/InGame/RaderMapExample/View.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RaderMapExample
+{
+    public class View : MonoBehaviour
+    {
+        [SerializeField] private Transform _parent;
+        [SerializeField] private Image _iconPrefab;
+        [SerializeField] private float _scale = 1.0f;
+        [SerializeField] private float _distance = 2.5f;
+
+        private Rader _rader;
+        private Image[] _icons;
+
+        void Start()
+        {
+            _rader = FindObjectOfType<Rader>();
+
+            // レーダーで捉える事が出来る限界と同じ数だけアイコンを生成しておく。
+            _icons = new Image[_rader.Capacity];
+            for (int i = 0; i < _icons.Length; i++)
+            {
+                _icons[i] = Instantiate(_iconPrefab, _parent);
+                _icons[i].transform.position = _parent.position;
+                _icons[i].enabled = false;
+            }
+        }
+
+        void Update()
+        {
+            DisableAllIcons();
+
+            foreach(Vector2 vec in _rader.GetResultVec2())
+            {
+                EnableIcon(vec);
+            }
+        }
+
+        private void DisableAllIcons()
+        {
+            foreach (Image i in _icons) i.enabled = false;
+        }
+
+        private void EnableIcon(Vector2 vec)
+        {
+            Vector3 offset = _parent.position;
+            Vector3 diff = (Vector3)vec * _scale;
+
+            if (diff.sqrMagnitude > _distance * _distance) return;
+
+            if (TryGetDisableIcon(out Image icon))
+            {
+                icon.transform.localPosition = diff;
+            }
+        }
+
+        private bool TryGetDisableIcon(out Image icon)
+        {
+            foreach (Image i in _icons)
+            {
+                if (!i.enabled) 
+                { 
+                    icon = i;
+                    icon.enabled = true;
+                    return true; 
+                }
+            }
+
+            icon = null;
+            return false;
+        }
+    }
+}
