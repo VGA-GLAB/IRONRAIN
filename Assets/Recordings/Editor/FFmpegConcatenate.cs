f877f855e59e571ab8f1b212c85972f4b96cfabd
VGA-TKY-DT24049\vantan <ryutoomori0414@gmail.com>
2024-08-27 15:44:10 +0900

    [add]Recorder機能追加

---
+++ Assets/Recordings/Editor/FFmpegConcatenate.cs
@@ -0,0 +1,59 @@
+﻿#if UNITY_EDITOR
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class FFmpegConcatenate : MonoBehaviour
+{
+    // 動画ファイルのディレクトリを指定
+    private string videoFilesDirectory = @"C:\Users\vantan\Desktop\IronRain\Assets\Recordings\Videos";
+
+    // 結合する動画ファイルのリスト
+    private string[] videoFiles = { "Part 1.mp4", "Part 2.mp4", "Part 3.mp4" };
+
+    public void ConcatenateVideos()
+    {
+        // ファイルリストを含むテキストファイルを作成
+        string fileListPath = Path.Combine(videoFilesDirectory, "filelist.txt");
+        using (StreamWriter sw = new StreamWriter(fileListPath))
+        {
+            foreach (string videoFile in videoFiles)
+            {
+                sw.WriteLine($"file '{Path.Combine(videoFilesDirectory, videoFile)}'");
+            }
+        }
+
+        // FFmpegコマンドの準備
+        string ffmpegPath = "ffmpeg"; // 環境変数に追加している場合
+        string arguments = $@"-f concat -safe 0 -i ""{fileListPath}"" -c copy -y ""{Path.Combine(videoFilesDirectory, "output.mp4")}""";
+
+        // FFmpegプロセスの開始
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = ffmpegPath,
+            Arguments = arguments,
+            RedirectStandardOutput = false,
+            RedirectStandardError = false,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            WindowStyle = ProcessWindowStyle.Hidden
+        };
+
+        using (Process process = new Process())
+        {
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        AssetDatabase.Refresh();
+    }
+
+    // 例として、Unityのボタンから呼び出すメソッド
+    public void Splice()
+    {
+        ConcatenateVideos();
+    }
+}
+#endif
---
+++ Assets/Recordings/Editor/UI/DebugUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IronRain.Recording
+{
+    public class DebugUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Recorder _recorder;
+
+        [SerializeField]
+        private TMPro.TextMeshProUGUI _label;
+        [SerializeField]
+        private Transform _image;
+
+        private void Update()
+        {
+            _image.gameObject.SetActive(_recorder.IsRecording);
+            _label.gameObject.SetActive(_recorder.IsRecording);
+
+            if (_recorder.IsRecording)
+            {
+                var y = Mathf.Sin(Time.time);
+                var x = Mathf.Cos(Time.time);
+                _image.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(y, x) * Mathf.Rad2Deg);
+            }
+        }
+    }
+}
