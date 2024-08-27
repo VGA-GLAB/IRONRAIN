5636e7df3cceef8b85497f53fd7d883197bbfaaf
Tappei <stutpi.signup@gmail.com>
2024-08-27 15:00:53 +0900

    [fix] 盾持ち雑魚の戦闘中の処理をボスと同じ設計に変更。

---
+++ Assets/InGame/Enemy/Scripts/Enemy/EnemyActionStep.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyActionStep : BattleActionStep
+    {
+        public EnemyActionStep(RequiredRef requiredRef, params EnemyActionStep[] next) : base(next)
+        {
+            Ref = requiredRef;
+        }
+
+        public RequiredRef Ref { get; }
+
+        protected override void Enter()
+        {
+        }
+
+        protected override BattleActionStep Stay()
+        {
+            return this;
+        }
+    }
+}
