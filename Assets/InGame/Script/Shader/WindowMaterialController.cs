082b62dfd9d5054e4bdd406e6cd089f2fe02c06f
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2024-08-29 19:11:07 +0900

    [fix] MonitorのCrackの変更

--- Assets/InGame/Script/Shader/WindowMaterialController.cs
+++ Assets/InGame/Script/Shader/WindowMaterialController.cs
@@ -7,23 +7,43 @@ namespace IronRain.ShaderSystem
 {
     public class WindowMaterialController : MonoBehaviour
     {
-        private Material[] _materials;
+        [Header("ひびのテクスチャ")]
+        [SerializeField] private Texture2D[] _centerTextures;
+        [SerializeField] private Texture2D[] _rightTextures;
+        [SerializeField] private Texture2D[] _leftTextures;
+        
+        [SerializeField] private Renderer _centerRenderer;
+        [SerializeField] private Renderer _rightRenderer;
+        [SerializeField] private Renderer _leftRenderer;
 
-        private int _crackAmountPropertyID = Shader.PropertyToID("_CrackAmount");
+        private readonly int _crackAmountPropertyID = Shader.PropertyToID("_CrackAmount");
+        private readonly int _crackTexturePropertyId = Shader.PropertyToID("_CrackTex");
+        
+        private CrackType currentCrack = CrackType.None;
+        
+        [ContextMenu("TestCrack")]
+        private void TestCrack()
+        {
+            currentCrack++;
+            currentCrack = currentCrack > CrackType.Type4 ? CrackType.None : currentCrack;
+            if (currentCrack is CrackType.None) return;
+            
+            Crack(currentCrack);
+        }
+
+        private void Start()
+        {
+            Crack(0F);
+        }
 
         /// <summary>日々の形基本的に1から4にかけて日々が増える</summary>
         public enum CrackType
         {
-            None,
-            Type1,
-            Type2,
-            Type3,
-            Type4
-        }
-
-        private void Awake()
-        {
-            _materials = Array.ConvertAll(GetComponentsInChildren<Renderer>(), renderer => renderer.material);
+            None = -1,
+            Type1 = 0,
+            Type2 = 1,
+            Type3 = 2,
+            Type4 = 3
         }
 
         /// <summary>モニターをヒビに入れる関数</summary>
@@ -32,20 +52,24 @@ namespace IronRain.ShaderSystem
         {
             crackRatio = Mathf.Clamp01(crackRatio);
             
-            foreach (var mat in _materials)
-            {
-                mat?.SetFloat(_crackAmountPropertyID, crackRatio);
-            }
+            _centerRenderer.sharedMaterial.SetFloat(_crackAmountPropertyID, crackRatio);
+            _rightRenderer.sharedMaterial.SetFloat(_crackAmountPropertyID, crackRatio);
+            _leftRenderer.sharedMaterial.SetFloat(_crackAmountPropertyID, crackRatio);
         }
 
         public void Crack(CrackType crackType)
         {
-            // Todo: テクスチャが来たらそれを差し替える処理に変更する
-            if (crackType is CrackType.None) return;
-            
-            foreach (var mat in _materials)
+            if (crackType is CrackType.None)
             {
-                mat?.SetFloat(_crackAmountPropertyID, 0.5F);
+                Crack(0F);
+            }
+            else
+            {
+                Crack(1F);
+                
+                _centerRenderer.sharedMaterial.SetTexture(_crackTexturePropertyId, _centerTextures[(int)crackType]);
+                _rightRenderer.sharedMaterial.SetTexture(_crackTexturePropertyId, _rightTextures[(int)crackType]);
+                _leftRenderer.sharedMaterial.SetTexture(_crackTexturePropertyId, _leftTextures[(int)crackType]);
             }
         }
     }
