da35f784cd1a403e7c676fb31d4d21e2668ae5d2
Tappei <stutpi.signup@gmail.com>
2024-08-31 13:22:47 +0900

    [fix] DebugPointPの修正。

---
+++ Assets/InGame/Script/PointP.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ボス戦のフィールドの中心。
+/// </summary>
+public class PointP : MonoBehaviour
+{
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 1.0f);
+    }
+}
