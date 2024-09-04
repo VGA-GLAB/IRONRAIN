52c48d496925ed5d6d425f70b0ae078de6e466a3
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2024-09-04 16:01:08 +0900

    [fix] Funnel後のResumeを追加

---
+++ Assets/InGame/Script/Sequence System/Sequence/EnemyManagerResumeBossActionSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Enemy;
+using UnityEngine;
+
+namespace IronRain.SequenceSystem
+{
+    public class EnemyManagerResumeBossActionSequence : ISequence
+    {
+        [OpenScriptButton(typeof(EnemyManagerResumeBossActionSequence))]
+        [Description("ポーズしたボスを再生させるSequence")]
+        [Header("このSequenceを抜けるまでの時間(秒)"), SerializeField] private float _totalSec = 0F;
+
+        private EnemyManager _enemyManager;
+
+        public void SetParams(float totalSec)
+        {
+            _totalSec = totalSec;
+        }
+
+        public void SetData(SequenceData data)
+        {
+            _enemyManager = data.EnemyManager;
+        }
+
+        public async UniTask PlayAsync(CancellationToken ct, Action<Exception> exceptionHandler = null)
+        {
+            _enemyManager.ResumeBossAction();
+
+            await UniTask.WaitForSeconds(_totalSec, cancellationToken: ct);
+        }
+
+        public void Skip()
+        {
+            _enemyManager.ResumeBossAction();
+        }
+    }
+}
