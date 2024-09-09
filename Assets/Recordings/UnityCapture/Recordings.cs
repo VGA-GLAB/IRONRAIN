b76d9ec92ae974949763e3a7f47cdc982bfc6545
VGA-TKY-DT24049\vantan <ryutoomori0414@gmail.com>
2024-09-09 14:15:08 +0900

    [add]画像録画機能

---
+++ Assets/Recordings/UnityCapture/Recordings.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// パワーシェルでコマンドを実行する機能
+/// </summary>
+public class Recordings : MonoBehaviour
+{
+    const string StartRecordBat = @"""C:\Users\vantan\Desktop\IronRain\Assets\Recordings\UnityCapture\RecordStart.bat""";
+    const string StopRecordBat = @"""C:\Users\vantan\Desktop\IronRain\Assets\Recordings\UnityCapture\RecordStop.bat""";
+
+    private Thread _startProcess = null;
+    Process[] processes;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (_startProcess != null && _startProcess.IsAlive)
+            {
+                UnityEngine.Debug.Log("Already running...");
+                return;
+            }
+
+            processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName == "obs64")
+                {
+                    return;
+                }
+            }
+
+            _startProcess = new Thread(StartRecord);
+            _startProcess.Start();
+            UnityEngine.Debug.Log("CaptureStart");
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Thread thread = new Thread(StopRecord);
+            thread.Start();
+            UnityEngine.Debug.Log("CaptureStop");
+        }
+    }
+
+    /// <summary>
+    /// AWSのS3に動画をアップロード機能
+    /// </summary>
+    /// <param name="action">終了時に実行したい処理があれば</param>
+    /// <param name="isHide">パワーシェルを出すかどうか</param>
+    public void PowerShellCommand(string batFile, bool isHide = true)
+    {
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+
+        if (isHide) startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+        startInfo.FileName = batFile;
+        startInfo.UseShellExecute = false;
+        startInfo.RedirectStandardError = true;
+        startInfo.CreateNoWindow = true;
+        process.StartInfo = startInfo;
+        process.Start();
+        process.WaitForExit();
+
+        UnityEngine.Debug.Log(process.StandardError.ReadLine());
+    }
+
+    ///<summary>録画開始</summary>
+    public void StartRecord()
+    {
+       // float timer = ;
+        PowerShellCommand(StartRecordBat);
+    }
+
+    ///<summary>録画停止</summary>
+    public void StopRecord()
+    {
+        PowerShellCommand(StopRecordBat);
+
+    }
+}
---
+++ Assets/Recordings/UnityRecorder/FFmpegConcatenate.cs
@@ -0,0 +1,79 @@
+﻿#if UNITY_EDITOR
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using System.Linq;
+#endif
+using UnityEngine;
+using Unity.VisualScripting;
+
+public class FFmpegConcatenate : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+
+    // private string videoFilesDirectory = @"C:\Users\vantan\Desktop\IronRain\Assets\Recordings\Videos";
+
+    // 動画ファイルのディレクトリを指定
+    private string videoFilesDirectory = @"C:\Users\vantan\Videos";
+
+    // 結合する動画ファイルのリスト
+    // private string[] videoFiles = { "Part 1.mp4", "Part 2.mp4", "Part 3.mp4" };
+    private string[] videoFiles => Directory.GetFiles(videoFilesDirectory,"*.mp4").
+                                    OrderBy(f => f).
+                                    TakeLast(3).
+                                    ToArray();
+
+    public void ConcatenateVideos()
+    {
+        // ファイルリストを含むテキストファイルを作成
+        string fileListPath = Path.Combine(videoFilesDirectory, "IronRainOutput\\filelist.txt");
+        using (StreamWriter sw = new StreamWriter(fileListPath))
+        {
+            foreach (string videoFile in videoFiles)
+            {
+                sw.WriteLine($"file '{Path.Combine(videoFilesDirectory, videoFile)}'");
+            }
+        }
+
+        // FFmpegコマンドの準備
+        string ffmpegPath = "ffmpeg"; // 環境変数に追加している場合
+        string arguments = $@"-f concat -safe 0 -i ""{fileListPath}"" -c copy -y ""{Path.Combine(videoFilesDirectory, "IronRainOutput\\output.mp4")}""";
+
+        // FFmpegプロセスの開始
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = ffmpegPath,
+            Arguments = arguments,
+            RedirectStandardOutput = false,
+            RedirectStandardError = false,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            WindowStyle = ProcessWindowStyle.Hidden
+        };
+
+        using (Process process = new Process())
+        {
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        AssetDatabase.Refresh();
+    }
+
+    [CustomEditor(typeof(FFmpegConcatenate))]
+    public class FFmpegConcatenateDrawer : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Test") && target is FFmpegConcatenate t)
+            {
+                t.ConcatenateVideos();
+            }
+        }
+    }
+#endif
+}
