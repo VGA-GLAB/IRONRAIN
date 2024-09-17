a8c033d2d325a1ac0afeafff586604b730d97a38
sakamotosyo <syo10150000@gmail.com>
2024-09-17 22:57:14 +0900

    [Update]現在shieldの敵が範囲内に入っているかどうか

--- Assets/InGame/Script/Actor/Player/PlayerQTE.cs
+++ Assets/InGame/Script/Actor/Player/PlayerQTE.cs
@@ -6,16 +6,20 @@ using UnityEngine.InputSystem;
 using System.Threading;
 using UniRx;
 using Enemy;
+using System;
 
 namespace IronRain.Player
 {
     public class PlayerQTE : PlayerComponentBase
     {
         public PlayerQTEModel QTEModel { get; private set; }
+        public bool IsEnterShield => _isEnterShield;
 
         [SerializeField] private PlayerQTEView _qteView;
 
         private System.Guid _guid;
+
+        private bool _isEnterShield;
 
         private void Awake()
         {
@@ -30,16 +34,40 @@ namespace IronRain.Player
 
         private void OnTriggerEnter(Collider other)
         {
-            if (QTEModel == null || _playerEnvroment.PlayerState.HasFlag(PlayerStateType.Inoperable)
-                || _playerEnvroment.PlayerState.HasFlag(PlayerStateType.NonTriggerQte)) return;
+            if (QTEModel == null || _playerEnvroment.PlayerState.HasFlag(PlayerStateType.Inoperable)) return;
             var enemyTypeReader = other.GetComponentInParent<EnemyController>();
             if (enemyTypeReader == null) return;
             //盾持ちの敵が入ってきたら
-            if (enemyTypeReader.Params.Type == EnemyType.Shield && other.tag == "QteTrigger")
+            if (enemyTypeReader.Params.Type == EnemyType.Shield
+                && other.tag == "QteTrigger"
+                && !_playerEnvroment.PlayerState.HasFlag(PlayerStateType.NonTriggerQte))
             {
                 _guid = enemyTypeReader.BlackBoard.ID;
                 QTEModel.StartQTE(_guid, QteType.NormalQte).Forget();
             }
+            else if(enemyTypeReader.Params.Type == EnemyType.Shield
+                && other.tag == "QteTrigger") 
+            {
+                _isEnterShield = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            var enemyTypeReader = other.GetComponentInParent<EnemyController>();
+            if (enemyTypeReader == null) return;
+            
+            if (enemyTypeReader.Params.Type == EnemyType.Shield
+            && other.tag == "QteTrigger")
+            {
+                _isEnterShield = false;
+            }
+        }
+
+        [ContextMenu("Qte")]
+        public void QteTest() 
+        {
+            QTEModel.StartQTE(Guid.Empty, QteType.NormalQte).Forget();
         }
     }
 }
