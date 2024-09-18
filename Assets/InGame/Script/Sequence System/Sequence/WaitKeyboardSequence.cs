32fe25846431802cefbaa727c0a3047858318945
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2024-09-18 15:47:09 +0900

    [fix] Sequenceの調整

---
+++ Assets/InGame/Script/Sequence System/Sequence/WaitKeyboardSequence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace IronRain.SequenceSystem
+{
+    public class WaitKeyboardSequence : AbstractWaitSequence
+    {
+        [OpenScriptButton(typeof(WaitKeyboardSequence))]
+        [Description("キーボードの入力をまつSequence")]
+        [Header("どのKeyの入力を待つのか"), SerializeField]
+        private UnityEngine.InputSystem.Key _key;
+
+        public override async UniTask PlayAsync(CancellationToken ct, Action<Exception> exceptionHandler = null)
+        {
+            // Loop処理を待っている
+            using var loopCts = new CancellationTokenSource();
+            using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(ct, loopCts.Token);
+
+            this.PlayWaitingSequenceAsync(linkedCts.Token, exceptionHandler).Forget();
+
+            await UniTask.WaitUntil(() => UnityEngine.InputSystem.Keyboard.current[_key].isPressed);
+
+            loopCts.Cancel();
+        }
+
+        public override void Skip()
+        {
+        }
+    }
+}
